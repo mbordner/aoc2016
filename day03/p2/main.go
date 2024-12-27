91c691b7f951a5019f4cad0e2339986d0cbcfdb6
Michael Bordner <mbordner@gmail.com>
2024-12-27 14:56:41 -0500

    day 03

---
+++ day03/p2/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mbordner/aoc2016/common"
+	"github.com/mbordner/aoc2016/common/file"
+	"regexp"
+)
+
+var (
+	reValues = regexp.MustCompile(`^\s*(\d+)\s+(\d+)\s+(\d+)$`)
+)
+
+func main() {
+	lines, _ := file.GetLines("../data.txt")
+	origValues := make([][]int, len(lines))
+	for i, line := range lines {
+		if reValues.MatchString(line) {
+			matches := reValues.FindStringSubmatch(line)
+			origValues[i] = []int{common.StrToA(matches[1]), common.StrToA(matches[2]), common.StrToA(matches[3])}
+		}
+	}
+
+	values := make([][]int, 0, len(origValues))
+	for y := 0; y <= len(origValues)-3; y += 3 {
+		tvalues := [][]int{{}, {}, {}}
+		for j := 0; j < 3; j++ {
+			for i := 0; i < 3; i++ {
+				tvalues[i] = append(tvalues[i], origValues[y+j][i])
+			}
+		}
+		values = append(values, tvalues...)
+	}
+
+	possibleTriangles := 0
+	for _, vals := range values {
+		dvals := append(vals, vals...)
+		possible := true
+		for i := 0; i < len(vals); i++ {
+			if dvals[i]+dvals[i+1] <= dvals[i+2] {
+				possible = false
+				break
+			}
+		}
+		if possible {
+			possibleTriangles++
+		}
+	}
+
+	fmt.Println(possibleTriangles)
+
+}
