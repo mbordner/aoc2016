aaf175040364529ba550480c5388ae2425ea96c4
Michael Bordner <mbordner@gmail.com>
2024-12-29 21:00:45 -0500

    day 13

--- day13/p1/main.go
+++ day13/p1/main.go
@@ -1,5 +1,77 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mbordner/aoc2016/common"
+)
 
+func main() {
+	params := []struct {
+		favNum   int
+		startPos common.Pos
+		goalPos  common.Pos
+	}{{
+		favNum:   10,
+		startPos: common.Pos{X: 1, Y: 1},
+		goalPos:  common.Pos{X: 7, Y: 4},
+	}, {
+		favNum:   1352,
+		startPos: common.Pos{X: 1, Y: 1},
+		goalPos:  common.Pos{X: 31, Y: 39},
+	}}
+	param := params[1]
+	startPos := param.startPos
+	goalPos := param.goalPos
+
+	queue := make(common.Queue[common.Pos], 0, 100)
+	prev := make(common.PosLinker)
+	visited := make(common.PosContainer)
+
+	var solution common.Positions
+
+	queue.Enqueue(startPos)
+	visited[startPos] = true
+
+	deltas := common.Positions{common.DN, common.DE, common.DS, common.DW}
+
+	for !queue.Empty() {
+		cur := *(queue.Dequeue())
+		if cur == goalPos {
+			solution = common.Positions{goalPos}
+			for p := prev[goalPos]; p != startPos; p = prev[p] {
+				solution = append(common.Positions{p}, solution...)
+			}
+			break
+		} else {
+			for _, d := range deltas {
+				p := cur.Add(d)
+				if p.X >= 0 && p.Y >= 0 && !isWall(p, param.favNum) {
+					if !visited.Has(p) {
+						visited[p] = true
+						prev[p] = cur
+						queue.Enqueue(p)
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Println(len(solution))
+
+}
+
+func isWall(p common.Pos, favNum int) bool {
+	v := p.X*p.X + 3*p.X + 2*p.X*p.Y + p.Y + p.Y*p.Y
+	v += favNum
+	c := count1Bits(v)
+	return c%2 != 0
+}
+
+func count1Bits(n int) int {
+	c := 0
+	for n != 0 {
+		n &= n - 1
+		c++
+	}
+	return c
 }
--- day13/p2/main.go
+++ day13/p2/main.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"fmt"
+	"github.com/mbordner/aoc2016/common"
+)
+
+type State struct {
+	p common.Pos
+	s int
+}
+
 func main() {
 
+	favNum := 1352
+	startPos := common.Pos{X: 1, Y: 1}
+
+	queue := make(common.Queue[State], 0, 100)
+	visited := make(common.PosContainer)
+
+	queue.Enqueue(State{p: startPos, s: 0})
+	visited[startPos] = true
+
+	deltas := common.Positions{common.DN, common.DE, common.DS, common.DW}
+
+	for !queue.Empty() {
+		cur := *(queue.Dequeue())
+		if cur.s < 50 {
+			for _, d := range deltas {
+				p := cur.p.Add(d)
+				if p.X >= 0 && p.Y >= 0 && !isWall(p, favNum) {
+					if !visited.Has(p) {
+						visited[p] = true
+						queue.Enqueue(State{p: p, s: cur.s + 1})
+					}
+				}
+			}
+		}
+
+	}
+
+	fmt.Println(len(visited))
+
+}
+
+func isWall(p common.Pos, favNum int) bool {
+	v := p.X*p.X + 3*p.X + 2*p.X*p.Y + p.Y + p.Y*p.Y
+	v += favNum
+	c := count1Bits(v)
+	return c%2 != 0
+}
+
+func count1Bits(n int) int {
+	c := 0
+	for n != 0 {
+		n &= n - 1
+		c++
+	}
+	return c
 }
