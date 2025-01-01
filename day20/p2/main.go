675328e7b0c29eb2b2bba1afe68eb2e634871e62
Michael Bordner <mbordner@gmail.com>
2025-01-01 01:51:56 -0500

    day 20

--- day20/p2/main.go
+++ day20/p2/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"github.com/mbordner/aoc2016/common/file"
+	"github.com/mbordner/aoc2016/common/ranges"
+	"regexp"
+	"strconv"
+)
+
+var (
+	reRange = regexp.MustCompile(`^(\d+)-(\d+)$`)
+)
+
 func main() {
 
+	rs := ranges.Collection[int64]{}
+
+	lines, _ := file.GetLines("../data.txt")
+	for _, line := range lines {
+		matches := reRange.FindStringSubmatch(line)
+		rs.Add(getIntVal(matches[1]), getIntVal(matches[2]))
+	}
+
+	fmt.Println(rs.ValuePairs()[1] + 1)
+
+}
+
+func getIntVal(s string) int64 {
+	val, _ := strconv.ParseInt(s, 10, 64)
+	return val
 }
