8ffe686fbc1f1880c29ef24f8614cbee179e29ff
Michael Bordner <mbordner@gmail.com>
2024-12-31 20:14:37 -0500

    day 18

--- day18/p1/main.go
+++ day18/p1/main.go
@@ -1,5 +1,79 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mbordner/aoc2016/common/file"
+	"strings"
+)
 
+const (
+	TRAP = '^'
+	SAFE = '.'
+)
+
+type Tiles []string
+
+func (t Tiles) Print() {
+	for _, row := range t {
+		fmt.Println(row)
+	}
+}
+
+func main() {
+	tiles := getTiles(getData("../data.txt"), 40)
+
+	sum := 0
+	for _, row := range tiles {
+		sum += strings.Count(row, ".")
+	}
+
+	fmt.Println(sum)
+}
+
+func getTiles(firstRow string, rows int) Tiles {
+	tiles := make(Tiles, rows)
+	tiles[0] = firstRow
+
+	for i := 1; i < rows; i++ {
+		row := make([]byte, len(firstRow))
+		pRow := []byte(tiles[i-1])
+		for j := range row {
+			l, c, r := false, false, false
+			if j > 0 && pRow[j-1] == TRAP {
+				l = true
+			}
+			if pRow[j] == TRAP {
+				c = true
+			}
+			if j < len(firstRow)-1 && pRow[j+1] == TRAP {
+				r = true
+			}
+			trap := false
+			if l && c && !r {
+				trap = true
+			}
+			if c && r && !l {
+				trap = true
+			}
+			if l && !c && !r {
+				trap = true
+			}
+			if r && !c && !l {
+				trap = true
+			}
+			if trap {
+				row[j] = TRAP
+			} else {
+				row[j] = SAFE
+			}
+		}
+		tiles[i] = string(row)
+	}
+
+	return tiles
+}
+
+func getData(f string) string {
+	content, _ := file.GetContent(f)
+	return strings.TrimSpace(string(content))
 }
--- day18/p2/main.go
+++ day18/p2/main.go
@@ -1,5 +1,67 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mbordner/aoc2016/common/file"
+	"strings"
+)
 
+const (
+	TRAP = '^'
+	SAFE = '.'
+)
+
+func main() {
+	firstRow := getData("../data.txt")
+
+	sum := strings.Count(firstRow, string(SAFE))
+	prevRow := firstRow
+	for i := 1; i < 400000; i++ {
+		row := getNextTileRow(prevRow)
+		sum += strings.Count(row, string(SAFE))
+		prevRow = row
+	}
+
+	fmt.Println(sum)
+}
+
+func getNextTileRow(prevRow string) string {
+	pRow := []byte(prevRow)
+	row := make([]byte, len(pRow))
+	for j := range row {
+		l, c, r := false, false, false
+		if j > 0 && pRow[j-1] == TRAP {
+			l = true
+		}
+		if pRow[j] == TRAP {
+			c = true
+		}
+		if j < len(prevRow)-1 && pRow[j+1] == TRAP {
+			r = true
+		}
+		trap := false
+		if l && c && !r {
+			trap = true
+		}
+		if c && r && !l {
+			trap = true
+		}
+		if l && !c && !r {
+			trap = true
+		}
+		if r && !c && !l {
+			trap = true
+		}
+		if trap {
+			row[j] = TRAP
+		} else {
+			row[j] = SAFE
+		}
+	}
+	return string(row)
+}
+
+func getData(f string) string {
+	content, _ := file.GetContent(f)
+	return strings.TrimSpace(string(content))
 }
